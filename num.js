b80da298b9b635d60a1956055bde21ef9560a2e5
Aqsa Gull <aqsabravegirl3@gmail.com>
2024-02-23 12:37:39 +0500

    first commit

---
+++ num.js
@@ -0,0 +1,39 @@
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([{
+        type: "number",
+        name: "NO1",
+        message: "write your number here:"
+    },
+    {
+        type: "number",
+        name: "NO2",
+        message: "write your number here:"
+    },
+    {
+        type: "list",
+        name: "operator",
+        message: "select your operator:",
+        choices: ["+", "*", "/", "-"]
+    }
+]);
+const { NO1, NO2, operator } = answer;
+if (NO1 && NO2 && operator) {
+    let result = 0;
+    if (operator == "+") {
+        result = NO1 + NO2;
+    }
+    else if (operator == "-") {
+        result = NO1 - NO2;
+    }
+    else if (operator == "/") {
+        result = NO1 / NO2;
+    }
+    else if (operator == "*") {
+        result = NO1 * NO2;
+    }
+    console.log("your result is", result);
+}
+else {
+    console.log("please select a valid operator:");
+}
+// console.log(answer)
